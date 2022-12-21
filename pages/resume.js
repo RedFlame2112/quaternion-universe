035bc02378cbf269a073a0a1d6532566668484ad
RedFlame2112 <sagnik3@illinois.edu>
2022-12-20 23:04:01 -0500

    Updated resume

--- pages/resume.js
+++ pages/resume.js
@@ -2,10 +2,10 @@ import PageLayout from "components/PageLayout";
 const Resume = () => {
   return ( 
     <PageLayout>
-      <a href = "https://drive.google.com/uc?export=download&id=1cllLt0rY4YjRE4rwG5iC2qxzE1C2gbSl">
+      <a href = "https://docs.google.com/document/d/1K8uWIxN112sgh31smBr5YJpWMkJBbgUoKTf5PtNsRXQ/export?format=pdf">
         <h1>Click here to download my resume!</h1>
       </a>
-      <h2>Last Updated: 12/20/2022</h2>
+      <h2>Last Updated: 12/17/2022</h2>
     </PageLayout>
   );
 }
