58b0ec22fd7eda4a303ee3a759d55aa2a8768a8a
RedFlame2112 <sagnik3@illinois.edu>
2023-09-02 17:14:13 -0500

    Updated resume

--- pages/resume.js
+++ pages/resume.js
@@ -2,12 +2,10 @@ import PageLayout from "components/PageLayout";
 const Resume = () => {
   return ( 
     <PageLayout>
-      <embed
-        src="Resume-9-02-2023.pdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      />
+      <a href = "https://docs.google.com/document/d/1m5Ww05BQ3KCiIJyg-ZxtGFdZJOVATADGKHAMrIH7dQw/export?format=pdf">
+        <h1>Click here to download my resume!</h1>
+      </a>
+      <h2>Last Updated: 09/02/2023</h2>
     </PageLayout>
   );
 }
