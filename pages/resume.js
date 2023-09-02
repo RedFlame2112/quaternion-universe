fb9f78a66c4f5431f8f00ba41c6129576e7dbb7e
RedFlame2112 <sagnik3@illinois.edu>
2023-09-02 17:05:51 -0500

    Updated resume and installed PDF viewer

--- pages/resume.js
+++ pages/resume.js
@@ -2,10 +2,12 @@ import PageLayout from "components/PageLayout";
 const Resume = () => {
   return ( 
     <PageLayout>
-      <a href = "https://docs.google.com/document/d/1K8uWIxN112sgh31smBr5YJpWMkJBbgUoKTf5PtNsRXQ/export?format=pdf">
-        <h1>Click here to download my resume!</h1>
-      </a>
-      <h2>Last Updated: 12/20/2022</h2>
+      <embed
+        src="Resume-9-02-2023.pdf"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+      />
     </PageLayout>
   );
 }
